83fe9c0f833b878757f48569fcfe139139b0ade0
kika1s1 <tamiratkebede120@gmail.com>
2024-07-10 21:50:37 +0300

    routing way is changed and private route is added to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import screen from "./screens/index";
-
+import { ToastContainer } from "react-toastify";
+import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/login" element={<screen.login />} />
-          <Route path="/employer_register" element={<screen.employerRegistration />} />
-          <Route path="/worker_register" element={<screen.workerRegistration />} />
-          <Route path="/" element={<screen.home />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <>
+      <ToastContainer />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,39 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./app/store.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import EmployerRegistration from "./screens/EmployerRegistration.jsx";
+import WorkerRegistration from "./screens/WorkerRegistration.jsx";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/employer_register" element={<EmployerRegistration />} />
+      <Route path="/worker_register" element={<WorkerRegistration />} />
+      {/* Registered users */}
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/hiddenpage" element={<HomeScreen />} />
+      </Route>
+      
+    </Route>
+  )
+);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <RouterProvider router={router} />
       </PersistGate>
     </Provider>
   </React.StrictMode>
