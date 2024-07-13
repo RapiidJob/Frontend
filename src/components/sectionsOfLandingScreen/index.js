150e4b28096fd044064d81efd3c9a1abd9782d8e
fasill <fasilhawultie19@gmail.com>
2024-07-13 13:46:52 +0300

    foter added

--- src/components/sectionsOfLandingScreen/index.js
+++ src/components/sectionsOfLandingScreen/index.js
@@ -2,10 +2,13 @@ import Hero from "./Hero";
 import HowItWorks from "./HowItWorks";
 import PopularJobs from "./PopularJobs";
 import LatestUpdates from "./LatestUpdates";
+import Footer from "./Footer";
+
 const section = {
     Hero,
     HowItWorks,
     PopularJobs,
     LatestUpdates,
+    Footer,
 }
 export default section
