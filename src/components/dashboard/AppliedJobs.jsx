0a015fa8332d86f5d401d5b133407c8a8b68bd7e
kika1s1 <tamiratkebede120@gmail.com>
2024-07-15 11:59:11 +0300

    jobs alert finsihed version

--- src/components/dashboard/AppliedJobs.jsx
+++ src/components/dashboard/AppliedJobs.jsx
@@ -1,134 +1,160 @@
-
+import { IoLocationOutline } from "react-icons/io5";
+import { TiTick } from "react-icons/ti";
 const AppliedJobs = () => {
   return (
-    <main className="flex-1 p-6">
+    <main className="flex-1 p-6 bg-white">
       <div className="mb-6">
         <h1 className="text-3xl font-semibold">Applied Jobs (23)</h1>
       </div>
       <div className="bg-white p-4 rounded shadow-sm">
         <table className="w-full text-left">
           <thead>
-            <tr>
-              <th className="py-2">Job</th>
-              <th className="py-2">Location</th>
-              <th className="py-2">Date Applied</th>
-              <th className="py-2">Status</th>
-              <th className="py-2">Action</th>
+            <tr className="bg-slate-50 text-slate-400">
+              <th className="py-2 uppercase">Jobs</th>
+              <th className="py-2 uppercase">Date Applied</th>
+              <th className="py-2 uppercase">Status</th>
+              <th className="py-2 uppercase">Action</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-black  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">Feb 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Completed</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-green-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">Feb 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Completed</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-red-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 ">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  Completed
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">Feb 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Completed</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
-            <tr>
-              <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-red-600 rounded-full mr-2"></span>
-                  Electronics Maintenance
-                </span>
-              </td>
-              <td className="py-2">Adama</td>
-              <td className="py-2">Dec 7, 2024 23:26</td>
-              <td className="py-2 text-green-600">Active</td>
-              <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
-                  View Details
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-red-600 rounded-full mr-2"></span>
-                  Electronics Maintenance
-                </span>
-              </td>
-              <td className="py-2">Adama</td>
-              <td className="py-2">Dec 7, 2024 23:26</td>
-              <td className="py-2 text-green-600">Active</td>
-              <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
-                  View Details
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-red-600 rounded-full mr-2"></span>
-                  Electronics Maintenance
-                </span>
-              </td>
-              <td className="py-2">Adama</td>
-              <td className="py-2">Dec 7, 2024 23:26</td>
-              <td className="py-2 text-green-600">Active</td>
-              <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
-                  View Details
-                </button>
-              </td>
-            </tr>
+
             {/* Add more job rows here */}
           </tbody>
         </table>
-        
       </div>
       <div className="flex justify-center mt-4">
-          <div className="flex space-x-2">
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">&lt;</button>
-            <button className="bg-blue-600 text-white py-1 px-3 rounded">01</button>
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">02</button>
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">03</button>
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">04</button>
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">05</button>
-            <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">&gt;</button>
-          </div>
+        <div className="flex space-x-2">
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            &lt;
+          </button>
+          <button className="bg-blue-600 text-white py-1 px-3 rounded">
+            01
+          </button>
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            02
+          </button>
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            03
+          </button>
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            04
+          </button>
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            05
+          </button>
+          <button className="bg-gray-300 text-gray-600 py-1 px-3 rounded hover:bg-gray-400">
+            &gt;
+          </button>
         </div>
+      </div>
     </main>
   );
 };
--- src/components/dashboard/JobsAlert.jsx
+++ src/components/dashboard/JobsAlert.jsx
@@ -1,41 +1,48 @@
-import { FaBriefcase, FaHeart, FaBell } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { FaRegBookmark } from "react-icons/fa";
+import { IoBagRemoveOutline, IoLocationOutline } from "react-icons/io5";
+import { TiTick } from "react-icons/ti";
+import { MdOutlineNotificationsActive } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const JobsAlert = () => {
   return (
-    <main className="flex-1 p-6">
+    <main className="flex-1 p-6 bg-white">
       <div className="mb-6">
         <h1 className="text-3xl font-semibold">Hello, Fahmi Dinsefa</h1>
-        <p className="text-gray-600">Here is your daily activities and job alerts</p>
+        <p className="text-gray-600">
+          Here is your daily activities and job alerts
+        </p>
       </div>
 
       {/* Stats */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-6">
-        <div className="bg-white p-4 rounded shadow-sm flex items-center">
-          <div className="text-blue-600">
-            <FaBriefcase size={32} />
-          </div>
+        <div className="bg-success p-4 rounded shadow-sm flex justify-between items-center">
           <div className="ml-4">
-            <h3 className="text-xl font-semibold">15</h3>
-            <p className="text-gray-600">Applied Jobs</p>
+            <h3 className="text-3xl font-semibold">15</h3>
+            <p className=" text-xl">Applied Jobs</p>
+          </div>
+          <div className="text-blue-600">
+            <span className="">
+              <IoBagRemoveOutline size={32} />
+            </span>
           </div>
         </div>
-        <div className="bg-white p-4 rounded shadow-sm flex items-center">
-          <div className="text-yellow-600">
-            <FaHeart size={32} />
-          </div>
+        <div className="bg-warning p-4 rounded shadow-sm flex justify-between items-center">
           <div className="ml-4">
-            <h3 className="text-xl font-semibold">4</h3>
+            <h3 className="text-3xl font-semibold">4</h3>
             <p className="text-gray-600">Favorite Jobs</p>
           </div>
-        </div>
-        <div className="bg-white p-4 rounded shadow-sm flex items-center">
-          <div className="text-green-600">
-            <FaBell size={32} />
+          <div className="text-yellow-600">
+            <FaRegBookmark size={32} />
           </div>
+        </div>
+        <div className="bg-success p-4 rounded shadow-sm flex justify-between items-center">
           <div className="ml-4">
-            <h3 className="text-xl font-semibold">44</h3>
+            <h3 className="text-3xl font-semibold">44</h3>
             <p className="text-gray-600">Job Alerts</p>
+          </div>
+          <div className="text-green-600">
+            <MdOutlineNotificationsActive size={32} />
           </div>
         </div>
       </div>
@@ -43,13 +50,21 @@ const JobsAlert = () => {
       {/* Profile Alert */}
       <div className="bg-red-500 p-4 rounded mb-6">
         <div className="flex items-center">
-          <img src="https://media.istockphoto.com/id/1407759041/photo/confident-happy-beautiful-hispanic-student-girl-indoor-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=3hNSNk_rbalqc_N2FH2MrtOgJaAOaoDebVnu7x5Qtd0=" alt="Profile" className="w-12 h-12 rounded-full mr-4" />
+          <img
+            src="https://media.istockphoto.com/id/1407759041/photo/confident-happy-beautiful-hispanic-student-girl-indoor-head-shot-portrait.jpg?s=1024x1024&w=is&k=20&c=3hNSNk_rbalqc_N2FH2MrtOgJaAOaoDebVnu7x5Qtd0="
+            alt="Profile"
+            className="w-12 h-12 rounded-full mr-4"
+          />
           <div>
-            <h3 className="text-white font-semibold">Your profile editing is not completed.</h3>
-            <p className="text-white">Complete your profile editing & build your custom Resume</p>
+            <h3 className="text-white font-semibold">
+              Your profile editing is not completed.
+            </h3>
+            <p className="text-white">
+              Complete your profile editing & build your custom Resume
+            </p>
           </div>
           <button className="ml-auto bg-white text-red-500 font-semibold py-2 px-4 rounde">
-            Edit Profile <span className='text-xl'>&rarr;</span>
+            Edit Profile <span className="text-xl">&rarr;</span>
           </button>
         </div>
       </div>
@@ -58,13 +73,14 @@ const JobsAlert = () => {
       <div className="bg-white p-4 rounded shadow-sm">
         <div className="flex justify-between items-center mb-4">
           <h3 className="text-xl font-semibold">Recently Applied</h3>
-          <Link to="/applied-jobs" className="text-blue-600 hover:underline">View All</Link>
+          <Link to="/applied-jobs" className="text-blue-600 hover:underline">
+            View All
+          </Link>
         </div>
         <table className="w-full text-left">
           <thead>
-            <tr>
+            <tr className="bg-slate-100">
               <th className="py-2">Job</th>
-              <th className="py-2">Location</th>
               <th className="py-2">Date Applied</th>
               <th className="py-2">Status</th>
               <th className="py-2">Action</th>
@@ -73,84 +89,185 @@ const JobsAlert = () => {
           <tbody>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-green-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">May 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Active</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-red-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">May 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Active</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-blue-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">May 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Active</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-black  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">May 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Active</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
             <tr>
               <td className="py-2">
-                <span className="flex items-center">
-                  <span className="w-3 h-3 bg-green-600 rounded-full mr-2"></span>
-                  Electrician
+                <div className="flex ">
+                  <div className="w-10 h-10 bg-green-600  mr-2"></div>
+                  <div>
+                    <p>
+                      Electrician{" "}
+                      <span className="text-xs bg-slate-100 rounded-lg px-2 py-1 text-blue-700">
+                        Full Time
+                      </span>
+                    </p>
+                    <span className="flex gap-1  items-center">
+                      <IoLocationOutline />
+                      <span className="text-slate-500 text-sm">
+                        Addis Ababa
+                      </span>
+                      <span className="text-slate-500 text-sm">
+                        ETB 2500/ month
+                      </span>
+                    </span>
+                  </div>
+                </div>
+              </td>
+              <td className="py-2">May 2, 2024 19:28</td>
+              <td className="py-2 text-green-600">
+                <span className="flex gap-2 justify-start items-center">
+                  <TiTick />
+                  active
                 </span>
               </td>
-              <td className="py-2">Addis Ababa</td>
-              <td className="py-2">May 2, 2024 19:28</td>
-              <td className="py-2 text-green-600">Active</td>
               <td className="py-2">
-                <button className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700">
+                <button className="bg-slate-200  py-1 px-3 rounded text-blue-700 hover:bg-slate-300 hover:text-blue-800">
                   View Details
                 </button>
               </td>
             </tr>
+
             {/* Repeat for other jobs */}
           </tbody>
         </table>
